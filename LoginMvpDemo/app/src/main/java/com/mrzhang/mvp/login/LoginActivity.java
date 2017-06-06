ae61cc97470770955c15685f3f512b5679887c87
ZHughZhang <zhangxiaoquandemo@gmail.com>
2017-06-06 14:07:51 +0800

    创建个登录class 和 导入 rx 三兄弟

---
+++ LoginMvpDemo/app/src/main/java/com/mrzhang/mvp/login/LoginActivity.java
@@ -0,0 +1,18 @@
+package com.mrzhang.mvp.login;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by MrZhang on 2017/6/6.
+ */
+
+public class LoginActivity extends AppCompatActivity {
+	@Override
+	protected void onCreate(@Nullable Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+
+
+}
